b9cf17e7177757b26657aec35b2c7d01ff0083ab
centuriin <centuriin@yandex.ru>
2023-05-14 19:27:36 +0300

    add course service

---
+++ MicroserviceCourse/CourseContext.cs
@@ -0,0 +1,37 @@
+﻿using MicroserviceCourse.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MicroserviceCourse;
+
+public class CourseContext : DbContext
+{
+    public CourseContext(DbContextOptions<CourseContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Course> Courses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Course>(entity =>
+        {
+            entity
+                .ToTable("courses")
+                .HasKey(e => e.Id);
+
+            entity
+                .Property(e => e.Id)
+                .HasColumnName("id");
+
+            entity
+                .Property(e => e.Name)
+                .HasColumnName("name");
+
+            entity
+                .Property(e => e.Disciplenes)
+                .HasColumnName("disciplenes");
+        });
+    }
+
+}
---
+++ MicroserviceCourse/Program.cs
@@ -0,0 +1,24 @@
+using MicroserviceCourse;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Add db
+builder.Services.AddDbContext<CourseContext>(options =>
+    options.UseNpgsql("Host=localhost;Port=5432;Database=course_db;Username=postgres;Password=admin;IncludeErrorDetail=true"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
